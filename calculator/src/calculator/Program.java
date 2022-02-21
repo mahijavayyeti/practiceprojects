62dffa593bab9d0d81f14a3ba0f7b7ff3fa60811
vayyetimahijamp <vayyetimahijamp@ip-172-31-26-73.ec2.internal>
2022-02-21 10:31:42 +0000

    3rd commit

--- calculator/src/calculator/Program.java
+++ calculator/src/calculator/Program.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		 double num1;
-	      double num2;
-	      double ans;
+		   double num1;
+		   //int n1 = (int)num1 (type casting is a method that converts a data type into another data type
+		   double num2;
+		   double ans;
 	      char op;
+	      //Static keyword is mainly used for memory management and it is used for a constant variable
+	    /*final double PI=3.1416;(final keyword is used to denote constant if we denote final
+	       then it can be accessed only once whether it is variable, method or classes*/
 	      Scanner reader = new Scanner(System.in);
+	      while(true) { //loopingstatements
 	      System.out.print("Enter two numbers: ");
 	      num1 = reader.nextDouble();
 	      num2 = reader.nextDouble();
 	      System.out.print("\nEnter an operator (+, -, *, /): ");
 	      op = reader.next().charAt(0);
-	      switch(op) {
+	       switch(op) {
 	         case '+': ans = num1 + num2;
 	            break;
 	         case '-': ans = num1 - num2;
@@ -23,7 +28,8 @@ public class Program {
 	            break;
 	         case '/': ans = num1 / num2;
 	            break;
-	      default: System.out.printf("Error! Enter correct operator");
+	         
+	         default: System.out.printf("Error! Enter correct operator");
 	         return;
 	      }
 	      System.out.print("\nThe result is given as follows:\n");
@@ -31,4 +37,5 @@ public class Program {
 
 	}
 
+}
 }
